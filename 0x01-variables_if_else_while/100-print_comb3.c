32f5c6db653a1e56e6209e87e9eb1a84c5bc0d4d
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-18 06:18:00 -0700

    print all possible different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int digit1;
 int digit2;
 for (digit1 = '0'; digit1 <= '9'; digit1++)
 {
-for (digit2 = '0'; digit2 <= '9'; digit2++)
+  for (digit2 = (digit1 + 1); digit2 <= '9'; digit2++)
 {
 putchar(digit1);
 putchar(digit2);
-if (!(digit1 == '9' && digit2 == '9'))
+if (digit1 != '8' || digit2 != '9')
 {
 putchar(',');
 putchar(' ');
