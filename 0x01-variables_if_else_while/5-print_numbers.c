8d192bb34cd2a72fcadf4c5988e2acba758df421
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-18 02:50:40 -0700

    print all numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ *main - print all numbers from 0 to 9
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%i", num);
+num++;
+}
+putchar('\n');
+return (0);
+}
