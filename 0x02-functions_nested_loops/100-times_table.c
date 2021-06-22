2bdadb47c9391bb24086e03a9fb21e85cdc98238
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-22 11:12:16 -0700

    print times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+/**
+ *print_times_table - print the timestable of a number
+ *@n: - the number whose timestable we will print
+ *
+ *Return: 0
+ */
+
+void print_times_table(int n)
+{
+int row;
+int column;
+int product;
+if (n >= 0 && <= 15)
+{
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = (row * column);
+if (column == 0)
+{
+_putchar('0' + product);
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+}
+if (product <= 9)
+{		
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + product);
+}	     
+else if (product > 9 && product < 1		{
+_putchar(' ');
+_putchar('0' + (product / 10));
+_putchar('0' + (product % 10));
+}
+else if (product >= 100)
+{
+_putchar('0' + (product / 100));
+_putchar('0' + ((product / 10) % 10));
+_putchar('0' + (product % 10));
+}
+}
+}
+_putchar('\n');
+}
+}
+}
+			   
+		  
+		  
+		   
