5375306757c92e5dc23a860ca8a946621cb853d6
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-22 06:37:04 -0700

    Modified

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,15 @@ void print_to_98(int n)
 {
 while (n < 98)
 {
-printf("%d", n);
+printf("%d, ", n);
 n++;
 }
 while (n > 98)
 {
-printf("%d", n);
+printf("%d, ", n);
 n--;
 }
-if (n == 0)
+if (n == 98)
 {
 printf("%d", n);
 }
