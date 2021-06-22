dd4739f0d3bb5c1a1aef4a6303f3c133a89481b9
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-22 12:29:59 -0700

    print total of multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ *main - print the total of all numbers that are multiples of 3 and 5
+ *Return: - 0
+ */
+
+int main(void)
+{
+int n = 1;
+int total = 0;
+while (n < 1024)
+{
+if ((n % 3 == 0) && (n % 5 == 0))
+total = total + n;
+n++;
+}
+}
+printf("%d\n", total);
+return (0);
+}
