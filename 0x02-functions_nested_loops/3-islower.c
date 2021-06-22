604dc19cea213846322014eca29e47cf3b1c3ef0
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-22 02:37:13 -0700

    check for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *_islower - checks whether a char is of lowercase
+ *@c - the character to check 
+ *Return: 1 if lowercase or 0 if otherwise
+ */
+
+
+int _islower(char c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
+
