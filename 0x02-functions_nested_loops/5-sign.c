12ae8584ac2795d8d19f3879b8e0b2237f4da886
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-22 08:49:16 -0700

    Modified

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- *sign.c - print the sign of a number
- *@n - the number to check
+ *print_sign.c - print the sign of a number
+ *@n: - the number to check
  *
  *Return: 0
  */
 
-int print_sign(int c)
+int print_sign(int n)
 {
-if (c > 0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if ( c == 0)
+else if (c == 0)
 {
 _putchar('0');
 return (0);
@@ -24,5 +24,4 @@ else if (c < 0)
 _putchar('-');
 return (-1);
 }
-_putchar('\n');
 }
