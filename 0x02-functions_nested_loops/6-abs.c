91377ec8f3dd195e9d8d39e2089e816913e6bde1
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-22 04:19:58 -0700

    print the absolute value of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_abs - print the absolute value of a number
+ *@int - the number
+ *
+ *Return: absolute value of number
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+return (0);
+}
