1882e87e56ba0e021a164516ec43a29421ed8b8a
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-23 02:02:07 -0700

    check function positive_or_negative()

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *main - checks whether the functions prints positive or negative depending on the integer
+ *Return: - 0
+ */
+
+int main(void)
+{
+int i = 0;
+positive_or_negative(i);
+return (0);
+}
