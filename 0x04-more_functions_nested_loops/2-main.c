240718e60ef4466e3daf17dc01fe84cb1f7331ab
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-24 06:21:07 -0700

    Modified

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a = 5;
+  int b = 9;
+
+  int result = mul(int a, int b);
+
+  return (result);
+}
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,4 @@
-#include "holberton"
+#include "holberton.h"
 
 /**
  *mul - multiplies two numbers
