a17156246ac0a851f8ae4e7ca56878949fb34616
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-24 08:26:59 -0700

    Modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,25 +15,15 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 5 == 0 && i % 3 == 0)
-{
 printf("%s", "FizzBuzz");
-}
 else if (i % 3 == 0)
-{
 printf("%s","Fizz");
-}
 else if (i % 5 == 0)
-{
 printf("%s", "Buzz");
-}
 else
-{
 printf("%d", i);
-}
 if (i != 100)
-{
 printf(" ");
-}
 }
 printf("\n");
 return (0);
