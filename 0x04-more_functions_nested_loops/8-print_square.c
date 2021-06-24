5fc05b6c19954375bcb6a462e337772e6a7a86cd
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-24 07:11:46 -0700

    Modified

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,7 +11,7 @@ void print_square(int size)
 int row, column;
 if (size > 0)
 {
-for (row = 1; row <= size; size++)
+for (row = 1; row <= size; row++)
 {
 for (column = 1; column <= size; column++)
 {
