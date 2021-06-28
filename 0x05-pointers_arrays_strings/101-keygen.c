c4a237df6e9b7a2a0069c34c63b12bae4dc6126d
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-28 11:50:52 -0700

    Initial commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,21 @@
 #include<stdio.h>
 
 /**
- *main - 
+ *main - generates random passwords for 101-crackme
+ *Rerurn: 0
+ */
+
+int main(void)
+{
+int total;
+char c;
+srand(time(NULL));
+while(total <= 2645)
+{
+c = rand() % 128;
+total += c;
+putchar(c);
+}
+putchar(2772 - total);
+return (0);
+}
