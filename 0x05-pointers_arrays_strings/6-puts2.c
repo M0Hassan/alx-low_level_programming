61230122c7cfc3bf6efa787ac6eab45ac67de7a9
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-28 05:02:43 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ *_strlen - gets the lenght of a string
+ *@str: - the string
+ *Return: the lenght
+ */
+
+
+void _strlen(char *str)
+{
+int len = 0;  
+while (*str != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+
+/**
+ *puts2 - prints every second character of a string followed by a a line
+ *@str: - the string
+ *Return: 0
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0' && i < _strlen(str); i+=2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
