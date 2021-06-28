7cbfa13dd3f8fbf5f4ecb3f23bc8b0d37fb4a1e6
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-28 01:11:03 -0700

    Modified

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: - the string
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int count;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+return count;
+}
