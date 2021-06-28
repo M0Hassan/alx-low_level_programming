7bac6c7d9a64af2794356973990259e65bbd0322
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-28 02:25:43 -0700

    Modified

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton"
+
+/**
+ *print_rev - print the reverse of a string
+ *@s: the string to reverse
+ *Return: 0
+ */
+
+void print_rev(char *s)
+{
+int len = 0;
+int i;
+while (*s != '\0')
+{
+len++;
+++s;
+}
+s--;
+for (i = len; i > 0; i--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
