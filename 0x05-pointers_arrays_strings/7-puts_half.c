123c205659c0646ffaf8f4c65cc2982f312a5e5c
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-28 05:48:47 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ *_strlen - gets the length of a string
+ *@str: - the string
+ *Return: 0
+ */
+
+int _strlen(char *str)
+{
+int len = 0;
+while (*str != '\0')
+{
+len++;
+str++;
+}
+}
+
+
+
+/**
+ *puts_half - prints half of the string followed by a line
+ *@str: - the string
+ *Return: 0
+ */
+
+void puts_half(char *str)
+{
+int idx;
+int len = (_strlen(str));
+if (len % 2 != 0)
+{
+idx = (len / 2) + 1;
+}
+else
+{
+idx = (len / 2);
+}
+while (idx < len)
+{
+_putchar(*(str + idx));
+idx++;
+}
+_putchar('\n');
+}
