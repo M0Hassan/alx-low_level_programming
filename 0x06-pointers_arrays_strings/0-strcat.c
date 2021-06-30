d570a10e0473b10acb8d2ebba37484dcce47f27c
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-06-30 08:14:23 -0700

    Concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *_strcat - concatenates two strings together
+ *@dest: the first string
+ *@src: the second string
+ *Return: concatenated string
+*/
+
+char *_strcat(char *dest, char *sr)
+{
+int i = 0;
+int j = 0;
+while(dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i+j] = src[j];j++;
+}
+dest[i+j] = '\0';
+return (dest);
+}
