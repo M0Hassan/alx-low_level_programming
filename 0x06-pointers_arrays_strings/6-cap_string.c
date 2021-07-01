73892d8936381ae66e82660809eb19cad4f6d241
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-07-01 05:44:20 -0700

    Modified

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  *cap_string - capitalizes all words o a string
- *@s - the string
+ *@s: - the string
  *Return: the string
  */
 
@@ -10,18 +10,18 @@ char *cap_string(char *s)
 {
 int i = 0;
 int j;
-while(s[i] != '\0')
+while (s[i] != '\0')
 {
 i++;
 }
 for (j = 0; j < i; j++)
 {
 if (s[j] == 32 || s[j] == 44 || s[j] == 59 || s[j] ==
-46 || s[j]== 33 || s[j] == 63 || s[j] ==
+46 || s[j] == 33 || s[j] == 63 || s[j] ==
 34 || s[j] == 40 || s[j] == 41 || s[j] == 123 ||
 s[j] == 125 || s[j] == 9 || s[j] == 10)
 {
-if ( s[j + 1] >= 97 && s[j + 1] <= 122)
+if (s[j + 1] >= 97 && s[j + 1] <= 122)
 {
 s[j + 1] = s[j + 1] - 32;
 }
