b2c27adc5635f0cdd948179723cbc6daf4c05886
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-07-09 04:55:22 -0700

    Initial commit

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+
+  int n;
+  n = _pow_recursion(5, 0);
+    printf("%d\n", n);
+    n = _pow_recursion(0, 15);
+    printf("%d/n", n);
+    n = _pow_recursion(1, 115),
+      printf("%d\n", n);
+    n = _pow_recursion(2, 5);
+    printf("%d\n", n);
+  return (0);
+}
