0f713a3ed1276fd8d31c1a58b86f3e9270319d6d
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-08-09 06:10:23 -0700

    initial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,48 @@
 #include "main.h"
-
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 /**
-* read_textfile - read a certain size and prints to std output
-* @filename: file to read from
-* @letters: size to read
-* Return: actual size read and printed
+* read_textfile - file that read and print a file
+* @filename: char pointer
+* @letters: structure
+* Return: int
 */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd; /* file descriptor */
-ssize_t n_read, n_wrote;
-char *buffer;
+int fd;
+char *buff;
+ssize_t rd;
+ssize_t wr;
 
 if (filename == NULL)
 return (0);
 
-/* open */
+/*read*/
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 
-/* malloc buffer */
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
+buff = malloc(sizeof(char) * letters);
+if (buff == NULL)
 return (0);
-
-/* read */
-n_read = read(fd, buffer, letters);
-if (n_read == -1)
+rd = read(fd, buff, letters);
+if (rd == -1)
 {
-free(buffer);
-close(fd);
+free(buff);
 return (0);
 }
+/*write */
+wr = write(STDOUT_FILENO, buff, rd);
 
-/* write */
-n_wrote = write(STDOUT_FILENO, buffer, n_read);
-if (n_wrote == -1)
+if (wr == -1)
 {
-free(buffer);
-close(fd);
+free(buff);
 return (0);
 }
-
+free(buff);
 close(fd);
-return (n_read);
-
+return (wr);
 }
