b41668c1b2ba2a6b1ebb74d483ce5a6941daf685
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-09-16 04:40:01 -0700

    returns the sum of all data in a linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - get sum of data in doubly linked list
- * @head: pointer to head of doubly linked list
- * Return: sum
- */
+* sum_dlistint - get sum of data in doubly linked list
+* @head: pointer to head of doubly linked list
+* Return: sum
+*/
 int sum_dlistint(dlistint_t *head)
 {
-  int sum = 0;
+int sum = 0;
 
-  if (!head)
-    return (sum);
+if (!head)
+return (sum);
 
-  while (head)
-    {
-      sum += head->n;
-      head = head->next;
-    }
-  return (sum);
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
