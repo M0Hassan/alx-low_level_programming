00a8059d58e66a1b5d16a6a686a034641dcdf846
root <aminmohamedadan@gmail.com>
2022-01-19 06:41:52 +0000

    the header file containing the funtion definitions

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+void array_print(int *array, size_t lo, size_t hi);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+
+#endif
